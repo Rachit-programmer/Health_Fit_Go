fa33092b272ef1c18c473b169e6b5e4b7bee3199
Rachit Baranwal <73416658+Rachit-programmer@users.noreply.github.com>
2023-08-19 00:40:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 function calculateBMI() {
     var weight = parseFloat(document.getElementById("weight").value);
     var height = parseFloat(document.getElementById("height").value);
-    var age = parseInt(document.getElementById("age").value);
 
     if (weight > 0 && height > 0) {
         var bmi = weight / ((height / 100) * (height / 100));
         document.getElementById("result").innerHTML = bmi.toFixed(2);
     }
 }
+
+function submitButton() {
+    var com = document.getElementById("comment").value;
+    var name = document.getElementById("name").value;
+    console.log(name + "," + com);
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("comment").value = "";
+    document.getElementById("name").value = "";
+
+}
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
